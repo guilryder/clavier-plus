8b3fc3a20dce401c5ce479453d9a678a0ef9772c
Guillaume Ryder <guillaume.ryder@f81bfa00-d365-064f-a3c0-6bd5a5a06232>
2010-03-20 18:37:46 +0000

    done: added Unicode release script

--- Distrib/release.bat
+++ Distrib/release.bat
@@ -12,6 +12,9 @@ mystart /wait Clavier.mus
 echo Generating Clavier64.zip...
 mystart /wait Clavier64.mus
 
+echo Generating ClavierUnicode.zip...
+mystart /wait ClavierUnicode.mus
+
 echo Generating ClavierSrc.zip...
 mystart /wait ClavierSrc.mus
 
@@ -21,7 +24,7 @@ mystart /wait /verb CompileWithIS Clavier.iss
 echo Generating ClavierSetup64.exe...
 mystart /wait /verb CompileWithIS Clavier64.iss
 
-for %%i in (Clavier.zip Clavier64.zip ClavierSrc.zip ClavierSetup.exe ClavierSetup64.exe) do move /y %%i E:\Web\utilfr\dn || echo Error: %%i
+for %%i in (Clavier.zip Clavier64.zip ClavierUnicode.zip ClavierSrc.zip ClavierSetup.exe ClavierSetup64.exe) do move /y %%i E:\Web\utilfr\dn || echo Error: %%i
 
 rd /s /q Sources
 
