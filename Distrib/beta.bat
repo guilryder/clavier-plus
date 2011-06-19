b19000dc1f8c28be6bae4c504ceecdb94ca42b62
Guillaume Ryder <guillaume.ryder@f81bfa00-d365-064f-a3c0-6bd5a5a06232>
2011-06-19 02:06:40 +0000

    done: removed source code packing from release scripts, changed source code location to Google Code in help files

--- Distrib/beta.bat
+++ Distrib/beta.bat
@@ -1,15 +1,8 @@
 @echo off
-cd /d %~dp0\..
-call CopyCode
-cd Distrib
-
-echo Generating ClavierBetaSrc.zip...
-start /wait ClavierBetaSrc.mus
+cd /d %~dp0
 
 echo Generating ClavierBeta.zip...
 del ClavierBeta.zip 2>nul
 start /wait ClavierBeta.mus
 
-for %%i in (ClavierBeta.zip ClavierBetaSrc.zip) do move /y %%i E:\Web\utilfr\dn || echo Error: %%i
-
-rd /s /q Sources
+for %%i in (ClavierBeta.zip) do move /y %%i E:\Web\utilfr\dn || echo Error: %%i
--- Distrib/release.bat
+++ Distrib/release.bat
@@ -1,7 +1,5 @@
 @echo off
-cd /d %~dp0\..
-call CopyCode
-cd Distrib
+cd /d %~dp0
 
 echo Copying help files...
 call release-help.bat
@@ -15,17 +13,12 @@ mystart /wait Clavier64.mus
 echo Generating ClavierUnicode.zip...
 mystart /wait ClavierUnicode.mus
 
-echo Generating ClavierSrc.zip...
-mystart /wait ClavierSrc.mus
-
 echo Generating ClavierSetup.exe...
 mystart /wait /verb CompileWithIS Clavier.iss
 
 echo Generating ClavierSetup64.exe...
 mystart /wait /verb CompileWithIS Clavier64.iss
 
-for %%i in (Clavier.zip Clavier64.zip ClavierUnicode.zip ClavierSrc.zip ClavierSetup.exe ClavierSetup64.exe) do move /y %%i E:\Web\utilfr\dn || echo Error: %%i
-
-rd /s /q Sources
+for %%i in (Clavier.zip Clavier64.zip ClavierUnicode.zip ClavierSetup.exe ClavierSetup64.exe) do move /y %%i E:\Web\utilfr\dn || echo Error: %%i
 
 pause
