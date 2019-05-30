28e0d9b691b6271dc1b7861f36f5d55da5aca594
Guillaume Ryder <guillaume@ryder.fr>
2019-05-29 23:55:03 -0700

    done: removed most donate button translations, for consistency and to save a few KBs

--- Resource.h
+++ Resource.h
@@ -117,7 +117,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         40067
 #define _APS_NEXT_CONTROL_VALUE         1061
 #define _APS_NEXT_SYMED_VALUE           104
